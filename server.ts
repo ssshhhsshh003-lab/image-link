8a4149efee3938a54e8656a0a0471ad09c87131e
Awias Yasin <ssshhhsshh003@gmail.com>
2026-08-13 21:54:37 +0500

    Update server.ts

--- server.ts
+++ server.ts
@@ -438,6 +438,7 @@ app.get('/', requireAuth, (req, res) => {
           const res = await fetch('/api-user-links?id=' + id, { method: 'DELETE' });
           if (res.ok) loadLinks();
         }
+      
 
         function copyLink(slug) {
           navigator.clipboard.writeText(location.origin + '/i/' + slug);
